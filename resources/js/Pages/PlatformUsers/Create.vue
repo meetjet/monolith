ca3653911b3a0c4ac10d3a071296ab8d7f10999e
Mike Shu <heliax44@gmail.com>
2022-07-07 16:57:53 +0400

    WD-8 refactoring: platform user management to the "form components" package and the "PageSection" component

--- resources/js/Pages/PlatformUsers/Create.vue
+++ resources/js/Pages/PlatformUsers/Create.vue
@@ -14,61 +14,33 @@
 
     <div class="py-12">
       <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-        <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg px-6 py-8">
-          <form @submit.prevent="submitHandler">
-            <div class="flex flex-wrap">
-              <!-- Name -->
-              <div class="pb-4 w-full">
-                <JetLabel for="name" :value="$t('Name')" />
-                <JetInput
-                  id="name"
-                  v-model="form.name"
-                  type="text"
-                  class="mt-1 block w-full"
-                  autocomplete="name"
-                  required
-                />
-                <JetInputError :message="form.errors.name" class="mt-2" />
-              </div>
-
-              <!-- Email -->
-              <div class="pb-4 w-full">
-                <JetLabel for="email" :value="$t('Email')" />
-                <JetInput
-                  id="email"
-                  v-model="form.email"
-                  type="text"
-                  class="mt-1 block w-full"
-                  autocomplete="email"
-                  required
-                />
-                <JetInputError :message="form.errors.email" class="mt-2" />
-              </div>
-
-              <!-- Password -->
-              <div class="pb-4 w-full">
-                <JetLabel for="password" :value="$t('Password')" />
-                <JetInput
-                  id="password"
-                  v-model="form.password"
-                  type="text"
-                  class="mt-1 block w-full"
-                  autocomplete="password"
-                  required
-                />
-                <JetInputError :message="form.errors.password" class="mt-2" />
-              </div>
-            </div>
-
-            <JetButton
-              class="mt-3"
-              :class="{ 'opacity-25': form.processing }"
-              :disabled="form.processing"
-            >
-              {{ $t("Create") }}
-            </JetButton>
-          </form>
-        </div>
+        <page-section>
+          <Form @submit.prevent="submitHandler" v-model="form">
+            <!-- Name -->
+            <Input
+              name="name"
+              :label="$t('Name')"
+              autocomplete="name"
+              autofocus
+              required
+            />
+            <!-- Email -->
+            <Input
+              name="email"
+              :label="$t('Email')"
+              autocomplete="email"
+              required
+            />
+            <!-- Password -->
+            <Input
+              name="password"
+              :label="$t('Password')"
+              autocomplete="password"
+              required
+            />
+            <Submit :label="$t('Create')" />
+          </Form>
+        </page-section>
       </div>
     </div>
   </AppLayout>
@@ -76,24 +48,27 @@
 
 <script>
 import AppLayout from "@/Layouts/AppLayout.vue";
+import PageSection from "@/Shared/PageSection.vue";
 import { Link } from "@inertiajs/inertia-vue3";
-import JetInput from "@/Jetstream/Input.vue";
-import JetInputError from "@/Jetstream/InputError.vue";
-import JetLabel from "@/Jetstream/Label.vue";
-import JetButton from "@/Jetstream/Button.vue";
+import { useForm } from "@inertiajs/inertia-vue3";
+import {
+  Form,
+  Input,
+  Submit,
+} from "@protonemedia/form-components-pro-vue3-tailwind3-simple";
 
 export default {
   components: {
     AppLayout,
+    PageSection,
     Link,
-    JetInput,
-    JetInputError,
-    JetLabel,
-    JetButton,
+    Form,
+    Input,
+    Submit,
   },
   data() {
     return {
-      form: this.$inertia.form({
+      form: useForm({
         name: null,
         email: null,
         password: null,
@@ -102,7 +77,7 @@ export default {
   },
   methods: {
     submitHandler() {
-      this.form.post(route("users.store"));
+      this.form.post(this.route("users.store"));
     },
   },
 };
--- resources/js/Pages/PlatformUsers/Edit.vue
+++ resources/js/Pages/PlatformUsers/Edit.vue
@@ -8,99 +8,53 @@
           >{{ $t("Platform users") }}
         </Link>
         /
-        {{ form.name }}
+        {{ editForm.name }}
       </h2>
     </template>
 
     <div class="py-12">
       <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
         <!-- Edit form -->
-        <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg px-6 py-8">
-          <form @submit.prevent="submitHandler">
-            <div class="flex flex-wrap">
-              <!-- Name -->
-              <div class="pb-4 w-full">
-                <JetLabel for="name" :value="$t('Name')" />
-                <JetInput
-                  id="name"
-                  v-model="form.name"
-                  type="text"
-                  class="mt-1 block w-full"
-                  autocomplete="name"
-                  required
-                />
-                <JetInputError :message="form.errors.name" class="mt-2" />
-              </div>
-
-              <!-- Email -->
-              <div class="pb-4 w-full">
-                <JetLabel for="email" :value="$t('Email')" />
-                <JetInput
-                  id="email"
-                  v-model="form.email"
-                  type="text"
-                  class="mt-1 block w-full"
-                  autocomplete="email"
-                  required
-                />
-                <JetInputError :message="form.errors.email" class="mt-2" />
-              </div>
-            </div>
-
-            <div class="mt-3 flex items-center">
-              <Link
-                v-if="$page.props.user.id !== entity.id"
-                class="text-red-500"
-                type="button"
-                @click="destroyHandler"
-              >
-                {{ $t("Delete") }}
-              </Link>
-              <JetButton
-                class="ml-auto"
-                :class="{ 'opacity-25': form.processing }"
-                :disabled="form.processing"
-              >
-                {{ $t("Save") }}
-              </JetButton>
-            </div>
-          </form>
-        </div>
+        <page-section>
+          <Form @submit.prevent="editHandler" v-model="editForm">
+            <!-- Name -->
+            <Input
+              name="name"
+              :label="$t('Name')"
+              autocomplete="name"
+              required
+            />
+            <!-- Email -->
+            <Input
+              name="email"
+              :label="$t('Email')"
+              autocomplete="email"
+              required
+            />
+            <Submit :label="$t('Save')" />
+          </Form>
+        </page-section>
         <!-- Change password form -->
-        <div
-          class="mt-6 bg-white overflow-hidden shadow-xl sm:rounded-lg px-6 py-8"
-        >
-          <div class="font-bold text-xl mb-4">
-            {{ $t("Change password") }}
-          </div>
-          <form @submit.prevent="changePasswordHandler">
-            <div class="flex flex-wrap items-center">
-              <span class="w-full md:w-1/2">
-                <JetInput
-                  id="password"
-                  v-model="changePasswordForm.password"
-                  type="text"
-                  class="w-full"
-                  autocomplete="email"
-                  required
-                />
-                <JetInputError
-                  :message="changePasswordForm.errors.password"
-                  class="mt-2"
-                />
-              </span>
-              <span class="w-full md:w-1/2 mt-7 md:mt-0">
-                <JetButton
-                  class="ml-0 md:ml-4"
-                  :class="{ 'opacity-25': changePasswordForm.processing }"
-                  :disabled="changePasswordForm.processing"
-                >
-                  {{ $t("Save") }}
-                </JetButton>
-              </span>
-            </div>
-          </form>
-        </div>
+        <PageSection :title="$t('Change password')">
+          <Form
+            @submit.prevent="changePasswordHandler"
+            v-model="changePasswordForm"
+          >
+            <Input
+              name="password"
+              :label="$t('Password')"
+              autocomplete="password"
+              required
+            />
+            <Submit :label="$t('Save')" />
+          </Form>
+        </PageSection>
+        <!-- Delete form -->
+        <PageSection v-if="$page.props.user.id !== entity.id">
+          <Link class="text-red-500" type="button" @click="destroyHandler">{{
+            $t("Delete")
+          }}</Link>
+        </PageSection>
       </div>
     </div>
   </AppLayout>
@@ -108,31 +62,34 @@
 
 <script>
 import AppLayout from "@/Layouts/AppLayout.vue";
+import PageSection from "@/Shared/PageSection.vue";
 import { Link } from "@inertiajs/inertia-vue3";
-import JetInput from "@/Jetstream/Input.vue";
-import JetInputError from "@/Jetstream/InputError.vue";
-import JetLabel from "@/Jetstream/Label.vue";
-import JetButton from "@/Jetstream/Button.vue";
+import { useForm } from "@inertiajs/inertia-vue3";
+import {
+  Form,
+  Input,
+  Submit,
+} from "@protonemedia/form-components-pro-vue3-tailwind3-simple";
 
 export default {
   components: {
     AppLayout,
+    PageSection,
     Link,
-    JetInput,
-    JetInputError,
-    JetLabel,
-    JetButton,
+    Form,
+    Input,
+    Submit,
   },
   props: {
     entity: Object,
   },
   data() {
     return {
-      form: this.$inertia.form({
+      editForm: useForm({
         name: this.entity.name,
         email: this.entity.email,
       }),
-      changePasswordForm: this.$inertia.form({
+      changePasswordForm: useForm({
         password: null,
       }),
       translations: {
@@ -141,12 +98,12 @@ export default {
     };
   },
   methods: {
-    submitHandler() {
-      this.form.put(route("users.update", this.entity.id));
+    editHandler() {
+      this.editForm.put(this.route("users.update", this.entity.id));
     },
     changePasswordHandler() {
       this.changePasswordForm.put(
-        route("users.change-password", this.entity.id),
+        this.route("users.change-password", this.entity.id),
         {
           onSuccess: () => this.changePasswordForm.reset(),
         }
