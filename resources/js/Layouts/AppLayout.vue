1af7bdc3488366b1b08123d6c5c321cf46124356
Zorca Orcinus <vs@zorca.org>
2022-07-11 10:36:29 +0300

    Auto stash before merge of "main" and "origin/main"

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -1,42 +1,9 @@
-<script setup>
-import { ref } from "vue";
-import { Inertia } from "@inertiajs/inertia";
-import { Head, Link } from "@inertiajs/inertia-vue3";
-import JetApplicationMark from "@/Jetstream/ApplicationMark.vue";
-import JetBanner from "@/Jetstream/Banner.vue";
-import JetDropdown from "@/Jetstream/Dropdown.vue";
-import JetDropdownLink from "@/Jetstream/DropdownLink.vue";
-import JetNavLink from "@/Jetstream/NavLink.vue";
-import JetResponsiveNavLink from "@/Jetstream/ResponsiveNavLink.vue";
-
-defineProps({
-  title: String,
-});
-
-const showingNavigationDropdown = ref(false);
-
-const switchToTeam = (team) => {
-  Inertia.put(
-    route("current-team.update"),
-    {
-      team_id: team.id,
-    },
-    {
-      preserveState: false,
-    }
-  );
-};
-
-const logout = () => {
-  Inertia.post(route("logout"));
-};
-</script>
-
 <template>
-  <div>
+  <div class="flex h-screen overflow-hidden">
     <Head :title="title" />
 
-    <JetBanner />
+    <!-- Sidebar -->
+    <Sidebar :sidebarOpen="sidebarOpen" @close-sidebar="sidebarOpen = false" />
 
     <div class="min-h-screen bg-gray-100">
       <nav class="bg-white border-b border-gray-100">
@@ -418,3 +385,37 @@ const logout = () => {
     </div>
   </div>
 </template>
+
+<script setup>
+import { ref } from "vue";
+import { Inertia } from "@inertiajs/inertia";
+import { Head, Link } from "@inertiajs/inertia-vue3";
+import JetApplicationMark from "@/Jetstream/ApplicationMark.vue";
+import Sidebar from "@/Shared/Sidebar.vue";
+import JetDropdown from "@/Jetstream/Dropdown.vue";
+import JetDropdownLink from "@/Jetstream/DropdownLink.vue";
+import JetNavLink from "@/Jetstream/NavLink.vue";
+import JetResponsiveNavLink from "@/Jetstream/ResponsiveNavLink.vue";
+
+defineProps({
+  title: String,
+});
+
+const showingNavigationDropdown = ref(false);
+
+const switchToTeam = (team) => {
+  Inertia.put(
+    route("current-team.update"),
+    {
+      team_id: team.id,
+    },
+    {
+      preserveState: false,
+    }
+  );
+};
+
+const logout = () => {
+  Inertia.post(route("logout"));
+};
+</script>
